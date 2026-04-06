0bb9e323b90b698d060c0156788ce69674a92190
ROGER RIBEIRO DE OLIVEIRA <25132662@dsn127m1247443.cfp127aluno.local>
2026-04-06 16:58:47 -0300

    atividade consumindo uma api em kotlin

---
+++ app/src/main/java/com/example/consumoapi/service/EnderecoService.kt
@@ -0,0 +1,20 @@
+package com.example.consumoapi.service
+
+import com.example.consumoapi.model.Endereco
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface EnderecoService {
+    @GET("{cep}/json/")
+    fun getEnderecoByCep(@Path("cep") cep : String): Call<List<Endereco>>
+    @GET("{uf}/{cidade}/{rua}/json/")
+    fun getEnderecoByUfCidadeRua(
+        @Path("uf") uf: String,
+        @Path("cidade") cidade: String,
+        @Path("rua") rua: String
+    ): Call<List<Endereco>>
+
+
+
+}
